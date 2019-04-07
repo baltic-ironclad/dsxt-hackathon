232248c04c14b98fba2a08a6c37ba91b568508a0
Diku-chan <kdv99@mail.ru>
2019-04-07 06:59:10 +0300

    Client side is almost completed

--- app/server.py
+++ app/server.py
@@ -1,20 +1,21 @@
 import socket
-import sys
+import json
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('localhost', 8080))
-server.listen(5)
+server.listen(1)
 
 while True:
     connection, address = server.accept()
+    raw = b''
 
     try:
         while True:
-            data = connection.recv(32)
-            if not data:
+            received = connection.recv(4096)
+            if not received:
                 break
-            connection.sendall(data)
-            print('Data received')
+            raw += received
     finally:
+        data = json.loads(raw.decode('utf-8'))
         connection.close()
-        print('Client disconnected')
+        print(data)
